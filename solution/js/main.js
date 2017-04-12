350856bb6b67c2f194c5c760fb21126299ce4729
Joanna Colson <joannacolson@gmail.com>
2017-04-12 16:34:06 -0700

    Wed 4:30pm updates

--- solution/js/main.js
+++ solution/js/main.js
@@ -1,12 +1,14 @@
+// function definitions
+
+
+
+
+
+
+// js executed once page has finished loading
 $(function() {
     console.log("jQuery loaded");
     // debugger;
-
-    // $("#projects").click(function (){
-    //                 $('html, body').animate({
-    //                     scrollTop: $("#div1").offset().top
-    //                 }, 2000);
-    //             });
 
     // add smooth scrolling to the nav's anchors
     $("nav").on('click', 'a', function(event) {
@@ -18,19 +20,35 @@ $(function() {
     });
 
     // add highlighting menu item when scrolling to each section
-    // $(window).scroll(function() {
-            //     var position = $(this).scrollTop();
+    $(window).on("scroll", function() {
+        var position = $(this).scrollTop();
+        // offset highlights the section before reaching the very top of the page.
+        var offset = 1;
 
-            //     $('.section').each(function() {
-            //         var target = $(this).offset().top;
-            //         var id = $(this).attr('id');
+        $(".section").each(function() {
+            var target = $(this).offset().top;
+            var id = $(this).attr("id");
 
-            //         if (position >= target) {
-            //             $('#navigation > ul > li > a').removeClass('active');
-            //             $('#navigation > ul > li > a[href=#' + id + ']').addClass('active');
-            //         }
+            if (position + offset >= target) {
+                $("#navigation > ul > li > a").removeClass("active");
+                $("#navigation > ul > li > a[href='#" + id + "']").addClass("active");
+            }
 
-            //     });
-            // });
+        });
+    });
+    // Fade in & out text loop - jQuery
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(4000)
+            .fadeOut(2000, showNextQuote);
+    }
+    showNextQuote();
+
+
 
 });
