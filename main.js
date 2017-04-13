96fbdbb2dcc907ea800dc6dd0ff22807dfd54b59
alainabuzas <alaina.buzas@gmail.com>
2017-04-13 13:32:47 -0700

    slow slide with anchors for nav and contact form

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 $(function() {
 
-    //  SLIDESHOW
+    // SLIDESHOW
     // when user clicks dot, go to the photo assigned to that dot in the slideshow
     // when user hovers on dot(or clicks ? ? ), effect of some kind
 
-    //  COMMENT FORM VALIDATION
+    // navigation
+    // if link in header is clicked, slowly scroll to that place on the homepage
+    // need to "animate" body to slide down
+    // //css?
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+
+
+    // CONTACT FORM SHOW
+    // when click arrow-down, contact form slides up from bottom
+
+    $(".fa-angle-down").on("click", function(event) {
+        event.preventDefault();
+        $("#contact-form").removeClass("hidden");
+        $("html, body").animate({
+            scrollTop: $("#intro").offset().top
+        }, 1500);
+    });
+
+    // COMMENT FORM VALIDATION
 
     $(".form").on("submit", function(event) {
 
@@ -35,6 +66,7 @@ $(function() {
                 $(formError[i]).hide();
             }
         }
+
     });
 
 });
