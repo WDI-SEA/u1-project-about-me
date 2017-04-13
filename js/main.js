b932ab30ffb57fbb999508f12892615b03bcd641
emaquino44 <prexy44@yahoo.com>
2017-04-13 16:31:16 -0700

    added sticky nav, contact me form and new background color

--- js/main.js
+++ js/main.js
@@ -1,9 +1,56 @@
-x = 0;
 $(document).ready(function() {
-    $("div").scroll(function() {
-        $("span").text(x += 1);
-    });
-    $("button").click(function() {
-        $("div").scroll();
+
+
+    $(window).scroll(function() {
+        console.log($(window).scrollTop());
+
+        if ($(window).scrollTop() > 280) {
+            $('.nav-bar').addClass('navbar-fixed');
+        }
+        if ($(window).scrollTop() < 281) {
+            $('.nav_bar').removeClass('navbar-fixed');
+        }
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//**********Slider**************************//
+//     var width = 720;
+//     var animationSpeed = 1000;
+//     var pause = 1000;
+//     var currentSlide = 1;
+
+//     var $slider = $('#slider');
+//     var $slideContainer = $slider.find('slides');
+//     var $slides = $slideContainer.find('slide');
+
+//     var interval;
+
+//     function startSlider() {
+//         interval = setInterval(function() {
+
+
+//             $sliderContainer.animate({ 'margin-left': '-=' + width }), animationspeed, fucntion(), currentSlide++;
+//             if (currentSlide === $slides.length) {
+//                 currentSlide = 1;
+//                 $sliderContainer.css('margin-left', 0);
+//             }
+//         });
+//     }
+
+// }, pause);
+
+// $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+
+
+// });
