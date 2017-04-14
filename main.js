906a86d0186e2669da2812636a4ffb4cd3faf742
alainabuzas <alaina.buzas@gmail.com>
2017-04-14 16:59:56 -0700

    gallery works

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ $(function() {
     // if link in header is clicked, slowly scroll to that place on the homepage
     // need to "animate" body to slide down
     // //css?
-
     $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -81,38 +80,6 @@ $(function() {
         }, 1500);
     });
 
-    // COMMENT FORM VALIDATION
-
-    $(".form").on("submit", function(event) {
-
-        var basicForm = ($(".basic-form"));
-        var formError = ($(".form-error"));
-
-        console.log(basicForm);
-
-        event.preventDefault();
-
-        for (var i = 0; i < basicForm.length; i++) {
-            if (!basicForm[i].value) {
-                $(basicForm[i]).addClass("error");
-                $(formError[i]).show();
-            }
-        }
-    });
-
-
-    $(".basic-form").on("keydown", function() {
-
-        var basicForm = ($(".basic-form"));
-        var formError = ($(".form-error"));
-
-        for (var i = 0; i < basicForm.length; i++) {
-            if (basicForm[i].value) {
-                $(basicForm[i]).removeClass("error");
-                $(formError[i]).hide();
-            }
-        }
-
-    });
+    $('selector').slippry();
 
 });
