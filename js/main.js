c75ab8b90fc9d43c42a96682a16c52c2cc14c808
emaquino44 <prexy44@yahoo.com>
2017-04-16 01:35:08 -0700

    added nav scrolltop

--- js/main.js
+++ js/main.js
@@ -1,60 +1,15 @@
-$(document).ready(function() {
-
-
-    $(window).scroll(function() {
-        console.log($(window).scrollTop());
-
-        if ($(window).scrollTop() > 280) {
-            $('.nav-bar').addClass('navbar-fixed');
-        }
-        if ($(window).scrollTop() < 281) {
-            $('.nav_bar').removeClass('navbar-fixed');
-        }
-    });
-});
-
-
-
 //*************SLIDER/CAROUSEL********************************//
 $(document).ready(function() {
     $(".slider").slick({
         dots: true
     });
+    $("nav a").click(function(e) {
+        var btnId = "section" + this.id;
+
+        $('html, body').animate({
+            scrollTop: $("#" + btnId).offset().top
+        }, 3000);
+    });
+
+
 });
-
-
-
-
-
-
-
-
-//     var width = 720;
-//     var animationSpeed = 1000;
-//     var pause = 1000;
-//     var currentSlide = 1;
-
-//     var $slider = $('#slider');
-//     var $slideContainer = $slider.find('slides');
-//     var $slides = $slideContainer.find('slide');
-
-//     var interval;
-
-//     function startSlider() {
-//         interval = setInterval(function() {
-
-
-//             $sliderContainer.animate({ 'margin-left': '-=' + width }), animationspeed, fucntion(), currentSlide++;
-//             if (currentSlide === $slides.length) {
-//                 currentSlide = 1;
-//                 $sliderContainer.css('margin-left', 0);
-//             }
-//         });
-//     }):
-
-// }, pause);
-
-// $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
-
-
-// });
