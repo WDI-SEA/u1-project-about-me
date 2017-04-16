b2193a406529cd690105068b99a5df2f58034b68
maria-christina-sevilla <m.christina.sevilla@gmail.com>
2017-04-16 15:34:46 -0700

    scrolling quotes

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ $(function() {
         $("#title").addClass('hidden');
     });
 
+    // desktop carousel
     $(".single-item").slick({
         dots: true
     });
+    // responsive carousel
 
 
     $(".submit").on('click', function(event) {
@@ -27,4 +29,33 @@ $(function() {
     });
     // hide title page
 
+});
+
+
+
+// end js
+
+
+function initQuoteCarousel() {
+
+    var $quotesWrapper = $(".cust-quotes");
+    var $quotes = $quotesWrapper.find("blockquote");
+
+    if (!$quotes.length) {
+        return;
+    }
+
+    var selectNextQuote = function() {
+        // keep move first quote in dom to the end to make continous
+        var $quote = $quotesWrapper.find("blockquote:first").detach().appendTo($quotesWrapper);
+
+        setTimeout(selectNextQuote, $quote.data("timeout"));
+    };
+
+    setTimeout(selectNextQuote, $quotes.filter(":first").data("timeout"));
+
+}
+
+$(function() {
+    initQuoteCarousel();
 });
